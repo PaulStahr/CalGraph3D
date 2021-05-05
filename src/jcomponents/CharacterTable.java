615a0f831375cbe88dddf83f19a67c8c791a54b9
Paul Stahr <paul.stahr@gmx.de>
2021-05-05 22:59:51 +0200

    Added minimal performance-optimization

--- src/jcomponents/CharacterTable.java
+++ src/jcomponents/CharacterTable.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (c) 2019 Paul Stahr
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,13 +21,10 @@
  ******************************************************************************/
 package jcomponents;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import data.DataHandler;
-import jcomponents.util.JComponentSingletonInstantiator;
-import util.JFrameUtils;
-
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -47,35 +44,37 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Toolkit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import data.DataHandler;
+import jcomponents.util.JComponentSingletonInstantiator;
+import util.JFrameUtils;
 
 /**
  * Write a description of class CharakterTable here.
- * 
+ *
  * @author Paul Stahr
  * @version 04.02.2012
  */
 public class CharacterTable extends JFrame implements ActionListener, KeyListener
 {
     /**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -906479873322525801L;
 	private static final Logger logger = LoggerFactory.getLogger(CharacterTable.class);
-	private static final JComponentSingletonInstantiator<CharacterTable> instantiator = new JComponentSingletonInstantiator<CharacterTable>(CharacterTable.class);
+	private static final JComponentSingletonInstantiator<CharacterTable> instantiator = new JComponentSingletonInstantiator<>(CharacterTable.class);
 	private final JPanel panelCharacterButtons = new JPanel(JFrameUtils.LEFT_FLOW_LAYOUT);
     private final JLabel textFieldDescription = new JLabel();
     private final JButton buttonCopy = new JButton("Kopieren");
     private char currentCharacter;
-    
+
     public static final ActionListener getOpenWindowListener()
     {
     	return instantiator;
     }
-    
+
     @Override
 	public void actionPerformed(ActionEvent ae){
     	Object source = ae.getSource();
@@ -104,7 +103,7 @@ public class CharacterTable extends JFrame implements ActionListener, KeyListene
     public CharacterTable(){
         final ButtonGroup characterButtonGroup = new ButtonGroup();
     	addKeyListener(this);
-    	
+
         setLayout(new GridBagLayout());
         try{
         	final InputStream stream = DataHandler.getResourceAsStream("/characters.txt");
@@ -115,8 +114,8 @@ public class CharacterTable extends JFrame implements ActionListener, KeyListene
             while (null!=(line = inBuf.readLine())){
                 try{
                     final int index = line.indexOf(':');
-                    final char c = parseCharacter(line.substring(line.indexOf('\'')+1, line.lastIndexOf('\'', index)));
-                    final JCharacterToggleButton characterToggleButton = new JCharacterToggleButton(String.valueOf(c), c);                    
+                    final char c = parseCharacter(line, line.indexOf('\'')+1, line.lastIndexOf('\'', index));
+                    final JCharacterToggleButton characterToggleButton = new JCharacterToggleButton(String.valueOf(c), c);
                     characterToggleButton.setDescription(line.substring(line.indexOf('"', index) + 1, line.lastIndexOf('"')));
                     characterToggleButton.addActionListener(this);
                     characterToggleButton.setPreferredSize(dim);
@@ -160,22 +159,22 @@ public class CharacterTable extends JFrame implements ActionListener, KeyListene
         pack();
         setBounds (100, 100, 350, 310);
         setTitle("Zeichentabelle");
-        setResizable(false);      
+        setResizable(false);
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         DataHandler.addToUpdateTree(this);
     }
-    
+
     public static final synchronized JFrame getInstance(){
     	return instantiator.get();
     }
-    
-    public static char parseCharacter(String str){
-    	if (str.length() == 1)
-    		return str.charAt(0);
-        if (str.length() == 6 && str.startsWith("\\u")){
+
+    public static char parseCharacter(String str, int begin, int end){
+    	if (end - begin == 1)
+    		return str.charAt(begin);
+        if (end - begin == 6 && str.charAt(begin) == '\\' && str.charAt(begin + 1) == 'u'){
         	int erg = 0;
-        	for (int i=2;i<6;i++){
+        	for (int i=2 + begin;i<end;i++){
         		final char c = str.charAt(i);
         		final int nextChar;
         		if (c >= '0' && c <= '9')
@@ -185,7 +184,7 @@ public class CharacterTable extends JFrame implements ActionListener, KeyListene
         		else if (c >= 'a' && c <= 'f')
         			nextChar = c-('a'-10);
         		else
-        	        throw new NumberFormatException("Can't read char:" + c);    
+        	        throw new NumberFormatException("Can't read char:" + c);
         		erg = (erg << 4) | nextChar;
         	}
         	return (char)erg;
@@ -198,7 +197,7 @@ public class CharacterTable extends JFrame implements ActionListener, KeyListene
 		private static final long serialVersionUID = -1706788315844600779L;
 		private String description;
 		private final char c;
-		
+
         public JCharacterToggleButton (String text, char c){super(text);this.c = c;}
 
         public void setDescription(String description){
