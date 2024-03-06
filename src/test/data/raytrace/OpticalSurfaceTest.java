c51a29ee954c3f16f0ae3fa72957228fd359f22c
Paul Stahr <paul.stahr@gmx.de>
2024-03-06 22:30:00 +0100

    Added test for Spherical OpticalSurfaceObject

--- src/test/data/raytrace/OpticalSurfaceTest.java
+++ src/test/data/raytrace/OpticalSurfaceTest.java
@@ -47,6 +47,34 @@ public class OpticalSurfaceTest {
     }
 
     @Test
+    public void testSpherical() throws OperationParseException {
+        RaytraceScene rs = new RaytraceScene("Spherical");
+        ParseUtil parser = new ParseUtil();
+        VariableStack va = new VariableStack();
+        GuiOpticalSurfaceObject goso = new GuiOpticalSurfaceObject(va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.SURFACE, SurfaceType.SPHERICAL, va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.DIRECTION, new Vector3d(1,0,0), va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.MINRADIUS, 0, va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.MAXRADIUS, 100, va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.MATERIAL, MaterialType.REFLECTION, va, parser);
+        goso.setValue(SCENE_OBJECT_COLUMN_TYPE.ACTIVE, true, va, parser);
+        rs.add(goso);
+        RaySimulationObject rso = new RaySimulationObject();
+        rs.updateScene();
+        for (int i = 0; i< 10; ++i)
+        {
+            rso.position.set(-10,i/100.,0);
+            Vector3d expectedPoint = new Vector3d(-Math.sqrt(1 - rso.position.y * rso.position.y),rso.position.y,0);
+            rso.direction.set(1, 0,0);
+            rso.numBounces = 0;
+            rs.calculateRay(rso, 10, null, 0, rs.copyActiveSurfaces(), null, 0);
+            rso.position.sub(expectedPoint);
+            rso.position.add(rso.direction, -rso.direction.dot(rso.position)/rso.direction.dot());
+            assertEquals(0,rso.position.norm(), 1E-5);
+        }
+    }
+
+    @Test
     public void testParabolic() throws OperationParseException {
         RaytraceScene rs = new RaytraceScene("Parabolic");
         ParseUtil parser = new ParseUtil();
