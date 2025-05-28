Fixed analytical surface-derivative

src/data/raytrace/OpticalSurfaceObject.py
@@ -180,15 +180,36 @@ class OpticalSurfaceObject(OpticalObject):
                 index += 1
         return res
 
-    def evaluate_inner_outer(self, position, normalize=True, xp=np):
+    @staticmethod
+    def numerical_derivative(function, delta):
+        def derivative(x, *args, **vargs):
+            num_points, dim = x.shape
+            testpoints = np.repeat(x[np.newaxis], dim * 2 + 1, axis=0)
+            dim = x.shape[1]
+            for i in range(dim):
+                testpoints[i * 2 + 1, :, i] -= delta
+                testpoints[i * 2 + 2, :, i] += delta
+            testpoints = testpoints.reshape(((dim * 2 + 1) * num_points, dim))
+            evaluated = function(testpoints, *args, **vargs)
+            evaluated = evaluated.reshape((dim * 2 + 1, num_points))
+            derivative = (evaluated[2::2] - evaluated[1::2]) / (2 * delta)
+            return evaluated[0], derivative.T
+        return derivative
+
+    def evaluate_inner_outer(self, position, normalize=False, xp=np):
         pos = position - xp.asarray(self.midpoint, dtype=position.dtype)
         surf = self.surf
 
         if surf == SurfaceType.FLAT:
-            return -self.directionNormalized.dot(pos)
+            result = -self.directionNormalized.dot(pos)
+            if normalize == 'seperate':
+                return result, np.repeat(self.directionNormalized[[np.newaxis] * (len(pos.shape) -1)], axis=0, repeats=pos.shape[:-1])
+            return result
         elif surf == SurfaceType.SPHERICAL:
             res = np.sum(np.square(pos), axis=-1)
             if normalize:
+                if normalize == 'seperate':
+                    return res - self.directionLengthQ, pos
                 np.sqrt(res, out=res)
                 return res - self.directionLength
             return res - self.directionLengthQ
@@ -199,11 +220,10 @@ class OpticalSurfaceObject(OpticalObject):
 
             res = posdot + self.conicConstant * np.square(dirdot) - self.directionLengthQ
             if normalize != False:
-                div = 2 * xp.sqrt(posdot - dirdot * (
-                        self.conicConstant * 2 * mdir - self.conicConstant ** 2 * dirdot))
+                div = 2 * pos - (2 * self.conicConstant) * dirdot[:, None] * self.directionNormalized[None, :]
                 if normalize == 'seperate':
                     return res, div
-                res /= div
+                res /= np.linalg.norm(div, axis=-1)
             return res
 
         elif surf == SurfaceType.CYLINDER:

src/test/data/raytrace/OpticalSurfaceObjectTest.py
@@ -1,30 +1,12 @@
 from calgraph3d.data.raytrace.OpticalSurfaceObject import OpticalSurfaceObject
 from calgraph3d.data.raytrace.SurfaceType import SurfaceType
 from calgraph3d.data.raytrace.Intersection import Intersection
-from scipy.misc import derivative
 import numpy as np
 import unittest
 from functools import partial
 
 
 class TestOpticalSurface(unittest.TestCase):
-    @staticmethod
-    def multiderivative(f, x, dx):
-        ndirs = len(x)
-        x = np.array(x, copy=True)
-        res = np.zeros(ndirs)
-        dxh = dx / 2
-        for i in range(ndirs):
-            orig = x[i]
-            x[i] = orig - dxh
-            a = f(x)
-            x[i] = orig + dxh
-            b = f(x)
-            x[i] = orig
-            res[i] = ((b - a) / dx)
-        return res
-
-
     def testInnerOuterCustom(self):
         rng = np.random.default_rng(seed=1)
         oso = OpticalSurfaceObject()
@@ -33,17 +15,15 @@ class TestOpticalSurface(unittest.TestCase):
         oso.direction[:] = (10,0,10)
         oso.midpoint[:] = (10,30,40)
         dx = 0.001
-        func = partial(oso.evaluate_inner_outer, normalize=False)
-        func_normed = partial(oso.evaluate_inner_outer, normalize=True)
+        num_testpoints = 100
+        rtol = 1e-8
+        location = rng.normal(0, 100, (num_testpoints, 3))
 
         for oso.conicConstant in (-1,0,2,3,4):
             oso.update()
-            for i in range(10):
-                location = np.round(rng.normal(0,100,3))
-                derivative = TestOpticalSurface.multiderivative(func, location, dx)
-                derivative /= oso.evaluate_inner_outer(location, normalize='seperate')[1]
-                np.testing.assert_allclose(np.linalg.norm(derivative), 1)
-                np.testing.assert_allclose(np.linalg.norm(TestOpticalSurface.multiderivative(func_normed, location, dx)), 0.5, atol=1.1)
+            derivative_numerical = OpticalSurfaceObject.numerical_derivative(oso.evaluate_inner_outer, dx)(location, normalize=False)[1]
+            derivative_analytical = oso.evaluate_inner_outer(location, normalize='seperate')[1]
+            np.testing.assert_allclose(derivative_numerical, derivative_analytical, rtol=rtol)
 
 
     def sphere_tests(self, oso):