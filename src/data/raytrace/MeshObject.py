ccdbbe48a066252bdd560c011782823fddd041c3
Paul Stahr <paul.stahr@gmx.de>
2026-03-15 18:29:00 +0100

    Added trimesh mesh intersection backend

--- src/data/raytrace/MeshObject.py
+++ src/data/raytrace/MeshObject.py
@@ -1,12 +1,17 @@
 import numpy as np
 import inspect
+import logging
 from calgraph3d.data.raytrace.OpticalObject import OpticalObject
 from calgraph3d.data.raytrace.Intersection import Intersection
 from jsymmath.geometry.Geometry import Geometry
 from jsymmath.util import ArrayUtil
 from jsymmath.geometry.AffineMatrix import AffineMatrix
 
+logger = logging.getLogger(__name__)
+
 class MeshObject(OpticalObject):
+    _printed_trimesh_engine = False
+
     def __init__(self):
         super().__init__()
         self.meshToGlobal = AffineMatrix(np.eye(4))
@@ -22,14 +27,30 @@ class MeshObject(OpticalObject):
         self.active = True
         self.smoothNormals = False
         self.radiusQ = np.nan
+        self.shading = True
+
+    def getColor(
+            self,
+            positions:np.ndarray=None,
+            textureCoordinate:np.ndarray=None,
+            xp=np):
+        result = ArrayUtil.convert(self.color, xp)
+        if self.texture is not None:
+            tCol = self.texture.getColor(textureCoordinate, xp)
+            if result is not None:
+                result = result * tCol
+            else:
+                result = tCol
+        return ArrayUtil.convert(result, xp)
 
     def getIntersection(self,
-                         position:np.ndarray,
-                         direction:np.ndarray,
-                         intersection:Intersection,
-                         lower_bound:np.ndarray,
-                         upper_bound:np.ndarray,
-                         xp=None):
+                        position:np.ndarray,
+                        direction:np.ndarray,
+                        intersection:Intersection,
+                        lower_bound:np.ndarray,
+                        upper_bound:np.ndarray,
+                        backend="trimesh",
+                        xp=None):
         if xp is None:
             xp = inspect.getmodule(type(position))
         position = self.globalToMesh.apply(position)
@@ -41,101 +62,199 @@ class MeshObject(OpticalObject):
         inv_dir_len = 1 / dir_len
         direction *= inv_dir_len[..., np.newaxis]
 
-        # Early bounding sphere check
-        weightRelative = position - ArrayUtil.convert(self.weightPoint[np.newaxis, :], xp)
-        distance = xp.sum(xp.square(weightRelative), axis=-1) - self.radiusQ
-        b_a = xp.sum(weightRelative * direction, axis=-1)
-        sqrt_a = xp.square(b_a) - distance
-        mask_b2a = xp.nonzero(sqrt_a >= 0)[0]
-        if len(mask_b2a) == 0:
-            return xp.zeros(shape=(0,), dtype=int)
-        sqrt_b = xp.sqrt(sqrt_a[mask_b2a])
-        b_b = b_a[mask_b2a]
-        mask_c2b = xp.nonzero((sqrt_b - b_b >= lower_bound[mask_b2a]) & (-sqrt_b - b_b <= upper_bound[mask_b2a]))[0]
-        if len(mask_c2b) == 0:
-            return xp.zeros(shape=[0], dtype=int)
-        mask_c2a = mask_b2a[mask_c2b]
-        assignment_mask  = xp.zeros(shape=len(position), dtype=bool)
-        vertices = ArrayUtil.convert(self.vertices, xp)
-        vertexNormals = ArrayUtil.convert(self.vertexNormals, xp)
-        faceNormals = ArrayUtil.convert(self.faceNormals, xp)
-        textureCoordinates = ArrayUtil.convert(self.textureCoordinates, xp)
-        faces = ArrayUtil.convert(self.faces, xp)
-        v0_all = vertices[faces[:, 0]]
-        v1_rel_v0_all = vertices[faces[:, 1]] - v0_all
-        v2_rel_v0_all = vertices[faces[:, 2]] - v0_all
-        d00_all = xp.sum(xp.square(v1_rel_v0_all), axis=-1)
-        d01_all = xp.sum(v1_rel_v0_all * v2_rel_v0_all, axis=-1)
-        d11_all = xp.sum(xp.square(v2_rel_v0_all), axis=-1)
-        inv_denom_all = 1 / (d00_all * d11_all - d01_all * d01_all)
-        direction_c = direction[mask_c2a]
-        position_c = position[mask_c2a]
-        lower_bound_c = lower_bound[mask_c2a]
+        # ---------------------------------------------------------
+        # Trimesh backend
+        # ---------------------------------------------------------
+        if backend == "trimesh":
+            import trimesh
 
-        for i in range(len(self.faces)):
-            face_normal = faceNormals[i]
-            v0 = v0_all[i]
+            # Create trimesh object once (cache if possible in your class!)
+            if not hasattr(self, "_trimesh"):
+                self._trimesh = trimesh.Trimesh(
+                    vertices=self.vertices,
+                    faces=self.faces,
+                    process=False
+                )
+                if not MeshObject._printed_trimesh_engine:
+                    engine_module = self._trimesh.ray.__module__
+                    pyembree = "pyembree" in engine_module
+                    logger.log(logging.INFO, f"Using trimesh ray tracing engine for MeshObject (trimesh version: {trimesh.__version__}) with {'Embree backend (fast)' if pyembree else 'pure NumPy backend'}")
 
-            pr_c = v0[np.newaxis, ...] - position_c
-            dist_c = (pr_c @ face_normal) / (direction_c @ face_normal)
+            # Perform ray intersection
+            locations, index_ray, index_tri = self._trimesh.ray.intersects_location(
+                ray_origins=ArrayUtil.convert(position, np),
+                ray_directions=ArrayUtil.convert(direction, np),
+                multiple_hits=False
+            )
 
-            mask_d2c = xp.nonzero((lower_bound_c <= dist_c) & (dist_c <= upper_bound[mask_c2a]))[0]
-            if len(mask_d2c) == 0:
-                continue
-            mask_d2a = mask_c2a[mask_d2c]
-            dist_d = dist_c[mask_d2c]
+            if len(index_ray) == 0:
+                return xp.zeros(shape=(0,), dtype=int)
 
-            location_rel_v0_d = dist_d[...,xp.newaxis] * direction[mask_d2a] - pr_c[mask_d2c]
+            locations = ArrayUtil.convert(locations, xp)
+            index_tri = ArrayUtil.convert(index_tri, xp)
+            index_ray = ArrayUtil.convert(index_ray, xp)
+            faceNormals = ArrayUtil.convert(self.faceNormals, xp)
+            faces = ArrayUtil.convert(self.faces, xp)
+            vertices = ArrayUtil.convert(self.vertices, xp)
+            vertexNormals = ArrayUtil.convert(self.vertexNormals, xp)
 
-            v1_rel_v0 = v1_rel_v0_all[i]
-            v2_rel_v0 = v2_rel_v0_all[i]
-            d00 = d00_all[i]
-            d01 = d01_all[i]
-            d11 = d11_all[i]
-            d20 = xp.sum(location_rel_v0_d * v1_rel_v0, axis=-1)
-            d21 = xp.sum(location_rel_v0_d * v2_rel_v0, axis=-1)
-            inv_denom = inv_denom_all[i]
-            u1_d = (d11 * d20 - d01 * d21) * inv_denom
-            u2_d = (d00 * d21 - d01 * d20) * inv_denom
+            # Compute distances in mesh space
+            hit_vec = locations - position[index_ray]
+            dist = xp.linalg.norm(hit_vec, axis=1)
 
-            u0_d = 1 - (u1_d + u2_d)
-            mask_e2d = xp.nonzero((u1_d >= 0) & (u2_d >= 0) & (u0_d >= 0))[0]
-            if len(mask_e2d) == 0:
-                continue
-            mask_e2a = mask_d2a[mask_e2d]
+            # Apply bounds filtering
+            valid_mask = (
+                    (dist >= lower_bound[index_ray]) &
+                    (dist <= upper_bound[index_ray])
+            )
 
-            # Hit found
-            intersection.position[mask_e2a] = location_rel_v0_d[mask_e2d] + position[mask_e2a]
-            u0_e = u0_d[mask_e2d]
-            u1_e = u1_d[mask_e2d]
-            u2_e = u2_d[mask_e2d]
-            vertex_indices = faces[i, :]
-            vertex_normals = vertexNormals[vertex_indices]
+            if not xp.any(valid_mask):
+                return xp.zeros(shape=(0,), dtype=int)
+
+            index_ray = index_ray[valid_mask]
+            index_tri = index_tri[valid_mask]
+            locations = locations[valid_mask]
+            dist = dist[valid_mask]
+
             if self.smoothNormals:
-                intersection.normal[mask_e2a] = (u0_e[:,xp.newaxis] * vertex_normals[0, xp.newaxis, :]
-                                             + u1_e[:,xp.newaxis] * vertex_normals[1, xp.newaxis, :]
-                                             + u2_e[:,xp.newaxis] * vertex_normals[2, xp.newaxis, :])
+                # Compute barycentric coordinates for normal interpolation
+                triangles = vertices[faces[index_tri]]
+                bary = trimesh.triangles.points_to_barycentric(
+                    triangles,
+                    locations
+                )
+
+                vertex_normals = vertexNormals[faces[index_tri]]
+
+                intersection.normal[index_ray] = (
+                        bary[:, 0:1] * vertex_normals[:, 0] +
+                        bary[:, 1:2] * vertex_normals[:, 1] +
+                        bary[:, 2:3] * vertex_normals[:, 2]
+                )
             else:
-                intersection.normal[mask_e2a] = face_normal[np.newaxis, :]
+                intersection.normal[index_ray] = faceNormals[index_tri]
 
-                if textureCoordinates is not None:
-                    intersection.textureCoords[mask_e2a] = (
-                            u2_e * textureCoordinates[vertex_indices[2]]
-                            + u1_e * textureCoordinates[vertex_indices[1]]
-                            + u0_e * textureCoordinates[vertex_indices[0]])
+            if self.textureCoordinates is not None:
+                textureCoordinates = ArrayUtil.convert(self.textureCoordinates, xp)
+                triangles_uv = textureCoordinates[faces[index_tri]]
+                bary = trimesh.triangles.points_to_barycentric(
+                    vertices[faces[index_tri]],
+                    locations
+                )
+                intersection.textureCoords[index_ray] = (
+                        bary[:, 0:1] * triangles_uv[:, 0] +
+                        bary[:, 1:2] * triangles_uv[:, 1] +
+                        bary[:, 2:3] * triangles_uv[:, 2]
+                )
+
+            # Update upper bounds
+            upper_bound[index_ray] = dist
+            intersection.distance[index_ray] = dist * inv_dir_len[index_ray]
+            intersection.object[index_ray] = self.id
+            intersection.faceIndex[index_ray] = index_tri
+
+            # Transform back to global
+            intersection.position[index_ray] = self.meshToGlobal.apply(locations)
+            intersection.normal[index_ray] = self.meshToGlobal.apply(intersection.normal[index_ray], only_linear=True)
+
+            return index_ray
+        if backend == "native":
+            # Early bounding sphere check
+            weightRelative = position - ArrayUtil.convert(self.weightPoint[np.newaxis, :], xp)
+            distance = xp.sum(xp.square(weightRelative), axis=-1) - self.radiusQ
+            b_a = xp.sum(weightRelative * direction, axis=-1)
+            sqrt_a = xp.square(b_a) - distance
+            mask_b2a = xp.nonzero(sqrt_a >= 0)[0]
+            if len(mask_b2a) == 0:
+                return xp.zeros(shape=(0,), dtype=int)
+            sqrt_b = xp.sqrt(sqrt_a[mask_b2a])
+            b_b = b_a[mask_b2a]
+            mask_c2b = xp.nonzero((sqrt_b - b_b >= lower_bound[mask_b2a]) & (-sqrt_b - b_b <= upper_bound[mask_b2a]))[0]
+            if len(mask_c2b) == 0:
+                return xp.zeros(shape=[0], dtype=int)
+            mask_c2a = mask_b2a[mask_c2b]
+            assignment_mask  = xp.zeros(shape=len(position), dtype=bool)
+            vertices = ArrayUtil.convert(self.vertices, xp)
+            vertexNormals = ArrayUtil.convert(self.vertexNormals, xp)
+            faceNormals = ArrayUtil.convert(self.faceNormals, xp)
+            textureCoordinates = ArrayUtil.convert(self.textureCoordinates, xp)
+            faces = ArrayUtil.convert(self.faces, xp)
+            v0_all = vertices[faces[:, 0]]
+            v1_rel_v0_all = vertices[faces[:, 1]] - v0_all
+            v2_rel_v0_all = vertices[faces[:, 2]] - v0_all
+            d00_all = xp.sum(xp.square(v1_rel_v0_all), axis=-1)
+            d01_all = xp.sum(v1_rel_v0_all * v2_rel_v0_all, axis=-1)
+            d11_all = xp.sum(xp.square(v2_rel_v0_all), axis=-1)
+            inv_denom_all = 1 / (d00_all * d11_all - d01_all * d01_all)
+            direction_c = direction[mask_c2a]
+            position_c = position[mask_c2a]
+            lower_bound_c = lower_bound[mask_c2a]
+
+            for i in range(len(self.faces)):
+                face_normal = faceNormals[i]
+                v0 = v0_all[i]
+
+                pr_c = v0[np.newaxis, ...] - position_c
+                dist_c = (pr_c @ face_normal) / (direction_c @ face_normal)
+
+                mask_d2c = xp.nonzero((lower_bound_c <= dist_c) & (dist_c <= upper_bound[mask_c2a]))[0]
+                if len(mask_d2c) == 0:
+                    continue
+                mask_d2a = mask_c2a[mask_d2c]
+                dist_d = dist_c[mask_d2c]
+
+                location_rel_v0_d = dist_d[...,xp.newaxis] * direction[mask_d2a] - pr_c[mask_d2c]
+
+                v1_rel_v0 = v1_rel_v0_all[i]
+                v2_rel_v0 = v2_rel_v0_all[i]
+                d00 = d00_all[i]
+                d01 = d01_all[i]
+                d11 = d11_all[i]
+                d20 = xp.sum(location_rel_v0_d * v1_rel_v0, axis=-1)
+                d21 = xp.sum(location_rel_v0_d * v2_rel_v0, axis=-1)
+                inv_denom = inv_denom_all[i]
+                u1_d = (d11 * d20 - d01 * d21) * inv_denom
+                u2_d = (d00 * d21 - d01 * d20) * inv_denom
+
+                u0_d = 1 - (u1_d + u2_d)
+                mask_e2d = xp.nonzero((u1_d >= 0) & (u2_d >= 0) & (u0_d >= 0))[0]
+                if len(mask_e2d) == 0:
+                    continue
+                mask_e2a = mask_d2a[mask_e2d]
+
+                # Hit found
+                intersection.position[mask_e2a] = location_rel_v0_d[mask_e2d] + position[mask_e2a]
+                u0_e = u0_d[mask_e2d]
+                u1_e = u1_d[mask_e2d]
+                u2_e = u2_d[mask_e2d]
+                vertex_indices = faces[i, :]
+                vertex_normals = vertexNormals[vertex_indices]
+                if self.smoothNormals:
+                    intersection.normal[mask_e2a] = (u0_e[:,xp.newaxis] * vertex_normals[0, xp.newaxis, :]
+                                                 + u1_e[:,xp.newaxis] * vertex_normals[1, xp.newaxis, :]
+                                                 + u2_e[:,xp.newaxis] * vertex_normals[2, xp.newaxis, :])
+                else:
+                    intersection.normal[mask_e2a] = face_normal[np.newaxis, :]
+
+                    if textureCoordinates is not None:
+                        intersection.textureCoords[mask_e2a] = (
+                                u2_e * textureCoordinates[vertex_indices[2]]
+                                + u1_e * textureCoordinates[vertex_indices[1]]
+                                + u0_e * textureCoordinates[vertex_indices[0]])
 
 
-            upper_bound[mask_e2a] = dist_d[mask_e2d]
-            intersection.object[mask_e2a] = self.id
-            intersection.faceIndex[mask_e2a] = i
-            assignment_mask[mask_e2a] = True
+                upper_bound[mask_e2a] = dist_d[mask_e2d]
+                intersection.object[mask_e2a] = self.id
+                intersection.faceIndex[mask_e2a] = i
+                assignment_mask[mask_e2a] = True
 
-        assignment_mask = xp.nonzero(assignment_mask)[0]
-        if len(assignment_mask) > 0:
-            intersection.position[assignment_mask] = self.meshToGlobal.apply(intersection.position[assignment_mask])
-            intersection.normal[assignment_mask] = self.meshToGlobal.apply(intersection.normal[assignment_mask], only_linear=True)
-            intersection.distance[assignment_mask] = upper_bound[assignment_mask] * inv_dir_len[assignment_mask]
-        return assignment_mask
+            assignment_mask = xp.nonzero(assignment_mask)[0]
+            if len(assignment_mask) > 0:
+                intersection.position[assignment_mask] = self.meshToGlobal.apply(intersection.position[assignment_mask])
+                intersection.normal[assignment_mask] = self.meshToGlobal.apply(intersection.normal[assignment_mask], only_linear=True)
+                intersection.distance[assignment_mask] = upper_bound[assignment_mask] * inv_dir_len[assignment_mask]
+            return assignment_mask
+        raise ValueError(f"Unknown backend '{backend}' for MeshObject ray intersection")
 
     def loadFile(self, fileName:str):
         import pymeshlab
